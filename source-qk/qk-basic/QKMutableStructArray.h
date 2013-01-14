10803b10ca0e86038dcc3f1ab6edd3d218a011a7
George King <george.w.king@gmail.com>
2013-01-14 14:39:48 -0500

    setEl: variants

--- source-qk/qk-basic/QKMutableStructArray.h
+++ source-qk/qk-basic/QKMutableStructArray.h
@@ -15,4 +15,13 @@
 - (void)replaceElementsInRange:(NSRange)range withBytes:(const void*)bytes count:(Int)count;
 - (void)removeElementsInRange:(NSRange)range;
 
+- (void)setEl:(int)index from:(void*)from;
+- (void)setEl:(int)index Int:(Int)val;
+- (void)setEl:(int)index I32:(I32)val;
+- (void)setEl:(int)index I64:(I64)val;
+- (void)setEl:(int)index F32:(F32)val;
+- (void)setEl:(int)index F64:(F64)val;
+- (void)setEl:(int)index V2F32:(V2F32)val;
+- (void)setEl:(int)index V2F64:(V2F64)val;
+
 @end
--- source-qk/qk-basic/QKMutableStructArray.m
+++ source-qk/qk-basic/QKMutableStructArray.m
@@ -46,9 +46,37 @@
                                  length:count * self.elSize];
 }
 
+
 - (void)removeElementsInRange:(NSRange)range {
   [self replaceElementsInRange:range withBytes:NULL count:0];
 }
+
+
+#define ASSERT_INDEX assert(index < self.count, @"bad index: %d; %@", index, self)
+
+- (void)setEl:(int)index from:(void*)from {
+  ASSERT_INDEX;
+  [self.mutableData replaceBytesInRange:[self byteRangeForIndex:index] withBytes:from];
+}
+
+
+#define SET_EL(T) \
+- (void)setEl:(int)index T:(T)val { \
+assert(self.elSize == sizeof(T), @"bad type: %s", #T); \
+ASSERT_INDEX; \
+[self.mutableData replaceBytesInRange:[self byteRangeForIndex:index] withBytes:&val]; \
+} \
+
+
+SET_EL(Int);
+SET_EL(I32);
+SET_EL(I64);
+SET_EL(F32);
+SET_EL(F64);
+SET_EL(V2F32);
+SET_EL(V2F64);
+
+
 
 
 @end
