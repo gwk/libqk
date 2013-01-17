88c052158a956e8c7f40e4e2fd3ecc2cde3e6fe8
George King <george.w.king@gmail.com>
2013-01-17 10:27:17 -0500

    comments

--- source-qk/qk-basic/qk-block-types.h
+++ source-qk/qk-basic/qk-block-types.h
@@ -16,26 +16,26 @@ typedef BOOL (^BlockFilter)(id);
 typedef id (^BlockMap)(id);
 
 // map a pair of values to a single value.
-typedef id (^BlockMapPair)(id, id);
+typedef id (^BlockMapPair)(id, id); // e.g. key, val
 
 // map an int to a value.
 typedef id (^BlockMapInt)(Int);
 
 // map a value and int to a value.
-typedef id (^BlockMapObjInt)(id, Int);
+typedef id (^BlockMapObjInt)(id, Int); // e.g. value, index
 
 // map a value to a pair of values.
 typedef Duo* (^BlockMapToPair)(id);
 
 // take the current aggregate value and the next value, and return the new aggregate value.
-typedef id (^BlockReduce)(id, id);
-typedef id (^BlockReducePair)(id, id, id);
+typedef id (^BlockReduce)(id, id); // agg, val
+typedef id (^BlockReducePair)(id, id, id); // e.g. agg, key, val
 
 // take a mutable aggregate value and the next value, and mutate the aggregate.
-typedef void (^BlockMutableReduce)(id, id);
-typedef void (^BlockMutableReducePair)(id, id, id);
+typedef void (^BlockMutableReduce)(id, id); // mutable_result, val
+typedef void (^BlockMutableReducePair)(id, id, id); // e.g. mutable_result, key, vanl
 
 // take a value and return an index.
 typedef Int (^BlockMaptoInt)(id);
 
-typedef Int(^BlockReduceToInt)(Int, id);
+typedef Int(^BlockReduceToInt)(Int, id); // agg, val
