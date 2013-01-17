a1e63df4c4050f3b3246dda5e5e284823f9c0350
George King <george.w.king@gmail.com>
2013-01-16 19:13:53 -0500

    fixed setupRootController window bounds

--- source-qk-ios/QKApplicationDelegate.m
+++ source-qk-ios/QKApplicationDelegate.m
@@ -15,11 +15,9 @@
 
 - (void)setupRootController:(UIViewController*)controller {
   _rootController = controller;
-  _window = [UIWindow withFrame:[[UIScreen mainScreen] applicationFrame]];
+  _window = [UIWindow withFrame:[[UIScreen mainScreen] bounds]]; // NOT applicationFrame, despite the documentation!
   _window.rootViewController = _rootController;
   [_window makeKeyAndVisible];
-  // sdk bug? makeKeyAndVisible seems to be setting view.frame = _window.frame, which is wrong.
-  _rootController.view.frame = _window.bounds;
 }
 
 
