2561b9859804fd993769128c2c888b2c7cd1125b
George King <george.w.king@gmail.com>
2013-01-19 09:28:35 -0500

    vec float functions

--- source-qk/qk-basic/qk-vec.h
+++ source-qk/qk-basic/qk-vec.h
@@ -7,7 +7,7 @@
 
 // define vector types as structs that contain an element array named '_' of length 'dim'.
 // this is preferable to named fields (e.g. 'x, y') because it makes the vector iterable;
-// this makes both generic macro code and application code more straightforward.
+// this makes both generic macro code and application code more regular.
 // the naming convention is VDT, where D is the dimension (e.g. '2'), and T is the scalar element type, (e.g. 'F32').
 // we make use of C compound literals, which look like type-casts of array literals (e.g, '(V2F32) { { 0, 0 } }').
 // this has the advantage that unspecified fields are zero-initialized,
@@ -15,39 +15,27 @@
 
 
 #define _TYPEDEF_V(TE, TV, dim) \
+\
 typedef struct { TE _[dim]; } TV; \
 \
-static inline TV TV##Add(TV a, TV b) { \
-TV r; for_in(i, dim) r._[i] = a._[i] + b._[i]; return r; \
-}\
+static const TV TV##Zero = (TV) {{}}; \
 \
-static inline TV TV##Sub(TV a, TV b) { \
-TV r; for_in(i, dim) r._[i] = a._[i] - b._[i]; return r; \
-} \
+static inline TV TV##Add(TV a, TV b) { TV r; for_in(i, dim) r._[i] = a._[i] + b._[i]; return r; } \
+static inline TV TV##Sub(TV a, TV b) { TV r; for_in(i, dim) r._[i] = a._[i] - b._[i]; return r; } \
 \
-static inline TV TV##Mul(TV v, TE s) { \
-TV r; for_in(i, dim) r._[i] = v._[i] * s; return r; \
-} \
+static inline TV TV##Mul(TV v, TE s) { TV r; for_in(i, dim) r._[i] = v._[i] * s; return r; } \
+static inline TV TV##Div(TV v, TE s) { TV r; for_in(i, dim) r._[i] = v._[i] / s; return r; } \
 \
-static inline TV TV##Div(TV v, TE s) { \
-TV r; for_in(i, dim) r._[i] = v._[i] / s; return r; \
-} \
+static inline TV TV##MulComps(TV a, TV b) { TV r; for_in(i, dim) r._[i] = a._[i] * b._[i]; return r; } \
+static inline TV TV##DivComps(TV a, TV b) { TV r; for_in(i, dim) r._[i] = a._[i] / b._[i]; return r; } \
 \
-static inline TV TV##MulComps(TV a, TV b) { \
-TV r; for_in(i, dim) r._[i] = a._[i] * b._[i]; return r; \
-} \
-\
-static inline TV TV##DivComps(TV a, TV b) { \
-TV r; for_in(i, dim) r._[i] = a._[i] / b._[i]; return r; \
-} \
-\
-static inline TV TV##Lerp(TV a, TV b, double t) { \
+static inline TV TV##Lerp(TV a, TV b, F64 t) { \
 TV r; for_in(i, dim) r._[i] = lerp(a._[i], b._[i], t); return r; \
 }\
 \
-static inline TV TV##WithCGPoint(CGPoint p) { return (TV) { { p.x, p.y } }; } \
+static inline TV TV##WithCGPoint(CGPoint p) { return (TV) {{ p.x, p.y }}; } \
 \
-static inline TV TV##WithCGSize(CGSize s) { return (TV) { { s.width, s.height } }; } \
+static inline TV TV##WithCGSize(CGSize s) { return (TV) {{ s.width, s.height }}; } \
 \
 static inline CGPoint CGPointWith##TV(TV v) { return CGPointMake(v._[0], v._[1]); } \
 \
@@ -57,7 +45,9 @@ static inline CGSize CGSizeWith##TV(TV v) { return CGSizeMake(v._[0], v._[1]); }
 #define _TYPEDEF_V2_EXP(TE, TV, fmt) \
 _TYPEDEF_V(TE, TV, 2) \
 \
-static inline TV TV##Make(TE x, TE y) { return (TV) { { x, y } }; } \
+static const TV TV##Unit = (TV) {{ 1, 1 }}; \
+\
+static inline TV TV##Make(TE x, TE y) { return (TV) {{ x, y }}; } \
 \
 static inline NSString* TV##Desc(TV v) { \
 return [NSString stringWithFormat:@"(" fmt @" " fmt @")", v._[0], v._[1]]; \
@@ -70,7 +60,7 @@ return (TV) { (v < r._[0] ? v : r._[0]), (v > r._[1] ? v : r._[1]) }; \
 
 #define _TYPEDEF_V3_EXP(TE, TV, fmt) \
 _TYPEDEF_V(TE, TV, 3) \
-static inline TV TV##Make(TE x, TE y, TE z) { return (TV) { { x, y, z } }; } \
+static inline TV TV##Make(TE x, TE y, TE z) { return (TV) {{ x, y, z }}; } \
 \
 static inline NSString* TV##Desc(TV v) { \
 return [NSString stringWithFormat:@"(" fmt @" " fmt @" " fmt @")", v._[0], v._[1], v._[2]]; \
@@ -79,25 +69,31 @@ return [NSString stringWithFormat:@"(" fmt @" " fmt @" " fmt @")", v._[0], v._[1
 
 #define _TYPEDEF_V4_EXP(TE, TV, fmt) \
 _TYPEDEF_V(TE, TV, 4) \
-static inline TV TV##Make(TE x, TE y, TE z, TE w) { return (TV) { { x, y, z, w } }; } \
+static inline TV TV##Make(TE x, TE y, TE z, TE w) { return (TV) {{ x, y, z, w }}; } \
 \
 static inline NSString* TV##Desc(TV v) { \
 return [NSString stringWithFormat:@"(" fmt @" " fmt @" " fmt @" " fmt @")", v._[0], v._[1], v._[2], v._[3]]; \
 } \
 
 
-#define _TYPEDEF_V2(TE, fmt) \
-_TYPEDEF_V2_EXP(TE, V2##TE, fmt)
+#define _TYPEDEF_V2(TE, fmt) _TYPEDEF_V2_EXP(TE, V2##TE, fmt)
+#define _TYPEDEF_V3(TE, fmt) _TYPEDEF_V3_EXP(TE, V3##TE, fmt)
+#define _TYPEDEF_V4(TE, fmt) _TYPEDEF_V4_EXP(TE, V4##TE, fmt)
 
-#define _TYPEDEF_V3(TE, fmt) \
-_TYPEDEF_V3_EXP(TE, V3##TE, fmt)
 
-#define _TYPEDEF_V4(TE, fmt) \
-_TYPEDEF_V4_EXP(TE, V4##TE, fmt)
+#define _DEF_V_F(dim, TV) \
+\
+static inline F64 TV##Dot(TV a, TV b) { F64 d = 0; for_in(i, dim) d += a._[i] * b._[i]; return d; } \
+\
+static inline F64 TV##Mag(TV v) { return sqrt(TV##Dot(v, v)); } \
+\
+static inline F64 TV##Dist(TV a, TV b) { return TV##Mag(TV##Sub(a, b)); } \
+\
+static inline TV TV##Norm(TV v) { return TV##Div(v, TV##Mag(v)); } \
 
 
 #define _DEF_V_F_WITH_I_EXP(dim, TF, TI, TVF, TVI) \
-static inline TVF TVF##With##TI(TVI v, double scale) { \
+static inline TVF TVF##With##TI(TVI v, F64 scale) { \
 TVF r; for_in(i ,dim) r._[i] = v._[i] / scale; \
 return r; \
 }
@@ -120,10 +116,23 @@ _TYPEDEF_V3(F64, @"%f");
 _TYPEDEF_V4(F32, @"%f");
 _TYPEDEF_V4(F64, @"%f");
 
+_DEF_V_F(2, V2F32);
+_DEF_V_F(2, V2F64);
+_DEF_V_F(3, V3F32);
+_DEF_V_F(3, V3F64);
+_DEF_V_F(4, V4F32);
+_DEF_V_F(4, V4F64);
+
 _DEF_V_F_WITH_I(2, F32, U16);
 
 
 typedef V2U16 Seg;
 typedef V3U16 Tri;
+
+
+// perpendicular vector
+static inline V2F32 V2F32Perp(V2F32 v) { return V2F32Make(v._[1], -v._[0]); }
+
+static inline V2F32 V2F32NormPerp(V2F32 v) { return V2F32Norm(V2F32Perp(v)); }
 
 
