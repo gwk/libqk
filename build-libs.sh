4281467360335ad27f81f00f525a293fe876dc1d
George King <george.w.king@gmail.com>
2013-03-04 14:56:10 -0500

    moved built products to ../libqk-built-*

--- build-libs.sh
+++ build-libs.sh
@@ -8,25 +8,27 @@ set -e
 
 error() { echo 'error:' "$@" 1>&2; exit 1; }
 
-[[ $0 == "./build-libs.sh" ]] || error "must invoke as ./build-libs.sh"
+[[ $(dirname $0) == "." ]] || error "must invoke as ./build-libs.sh"
 
 (( ${#@} >= 3 )) || error "usage: sqlite3 png jpeg-turbo [--quiet]"
 
 sqlite3=$1;   shift
 png=$1;       shift
 turbojpeg=$1;  shift
-export QUIET=$1;
+export QUIET=$1; shift
 
 libqk="$PWD"
 
-# note: string compare is not great, but catches the common case.
+# note: version string compare is not great, but catches the common case.
 nasm_version_req='version 2.10.00'
 nasm_version=$(nasm -v | egrep --only-matching --max-count=1 'version [0-9]+(\.[0-9]+)*')
 echo "nasm $nasm_version (above $nasm_version_req required)"
 [[ "$nasm_version" > "$nasm_version_req" ]] || error "modern nasm required: $nasm_version_req; using $(nasm -v)"
 
-# libjpeg-turbo requires gas-preprocessor, obtained from https://github.com/yuvi/gas-preprocessor
-[[ -r "$libqk/tools/gas-preprocessor.pl" ]] || error "missing tools/gas-preprocessor.pl"
+# libjpeg-turbo requires gas-preprocessor
+[[ -r "$libqk/tools/gas-preprocessor.pl" ]] \
+|| error "missing tools/gas-preprocessor.pl (obtained from obtained from https://github.com/yuvi/gas-preprocessor)"
+
 export PATH="$libqk/tools:$PATH"
 
 build_lib() {
@@ -40,7 +42,7 @@ build_lib() {
   local d="libs-$platform/$name"
   [[ -d "$d" ]] && rm -rf "$d"
   mkdir -p "$d"
-  ./build-lib-$platform.sh $cc_name $cc_opt lib$name "$path" built-$platform/$name $config_args
+  ./build-lib-$platform.sh $cc_name $cc_opt lib$name "$path" ../libqk-built-$platform/$name $config_args
   echo
 }
 
