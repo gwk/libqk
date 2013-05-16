50fcf133117ad692d265b25aac2eb1fe06b1f944
George King <george.w.king@gmail.com>
2013-05-15 21:46:36 -0400

    test-qk-ios

---
+++ test-qk-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  libqk-ios
+//
+//  Created by George King on 5/15/13.
+//  Copyright (c) 2013 George King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test-qk-ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  libqk-ios
+//
+//  Created by George King on 5/15/13.
+//  Copyright (c) 2013 George King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
