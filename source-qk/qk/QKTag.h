f638a85d9d71a264018550eca190d4e5647aef14
George King <george.w.king@gmail.com>
2013-05-22 09:28:26 -0400

    QKTag

---
+++ source-qk/qk/QKTag.h
@@ -0,0 +1,15 @@
+// Copyright 2013 George King.
+// Permission to use this file is granted in libqk/license.txt.
+
+
+#import "qk-macros.h"
+
+
+@interface QKTag : NSObject
+
+@property (nonatomic) int tag;
+@property (nonatomic) id obj;
+
+DEC_INIT(Tag:(int)tag obj:(id)obj);
+
+@end
---
+++ source-qk/qk/QKTag.m
@@ -0,0 +1,19 @@
+// Copyright 2013 George King.
+// Permission to use this file is granted in libqk/license.txt.
+
+
+#import "QKTag.h"
+
+
+@implementation QKTag
+
+
+DEF_INIT(Tag:(int)tag obj:(id)obj) {
+  INIT(super init);
+  _tag = tag;
+  _obj = obj;
+  return self;
+}
+
+
+@end
