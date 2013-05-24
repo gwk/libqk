0aa22332f4f920455533fe233fa6791d5d9e2432
George King <george.w.king@gmail.com>
2013-05-24 10:06:00 -0400

    QKTextLayer (empty)

---
+++ source-qk/qk-ca/QKTextLayer.m
@@ -0,0 +1,14 @@
+// Copyright 2013 George King.
+// Permission to use this file is granted in libqk/license.txt.
+
+
+#import "QKTextLayer.h"
+
+
+@interface QKTextLayer ()
+@end
+
+
+@implementation QKTextLayer
+@end
+
