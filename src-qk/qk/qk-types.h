2038efc4880c6563daf4da05ed2757c2514aeaf2
George King <george.w.king@gmail.com>
2014-04-12 01:14:24 -0400

    min_ and max_ constants.

--- src-qk/qk/qk-types.h
+++ src-qk/qk/qk-types.h
@@ -40,10 +40,18 @@ typedef char* AsciiM;
 typedef char* Utf8M;
 typedef wchar_t* Utf32M;
 
-
 static const Int min_Int = LONG_MIN;
 static const Int max_Int = LONG_MAX;
 static const Uns max_Uns = ULONG_MAX;
+static const I8 min_I8 = SCHAR_MIN;
+static const I8 max_I8 = SCHAR_MAX;
+static const U8 max_U8 = UCHAR_MAX;
+static const I16 min_I16 = SHRT_MIN;
+static const I16 max_I16 = SHRT_MAX;
+static const U16 max_U16 = USHRT_MAX;
+static const I32 min_I32 = INT_MIN;
+static const I32 max_I32 = INT_MAX;
+static const U32 max_U32 = UINT_MAX;
 static const I64 min_I64 = LLONG_MIN;
 static const I64 max_I64 = LLONG_MAX;
 static const U64 max_U64 = ULLONG_MAX;
